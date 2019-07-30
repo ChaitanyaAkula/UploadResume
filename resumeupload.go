8296135525cc7709100ddc51e9f7f4bd215ce973
ChaitanyaAkula <krishchaituakula@gmail.com>
2019-07-30 21:31:14 +0530

    Add files via upload

---
+++ resumeupload.go
@@ -0,0 +1,35 @@
+package resumeupload
+import(
+	"fmt"
+	"net/http"
+	"os"
+	"io"
+	"path/filepath"
+	"crypto/sha1"
+	"strings"
+    "mime/multipart"
+)
+
+
+
+func SetResume(w http.ResponseWriter,mf multipart.File,fh *multipart.FileHeader)string{
+
+	defer mf.Close()
+	ext := strings.Split(fh.Filename, ".")[1]
+	h := sha1.New()
+	io.Copy(h, mf)
+	fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	path := filepath.Join(wd, "public", "developerresume", fname)
+	nf, err := os.Create(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer nf.Close()
+	mf.Seek(0, 0)
+	io.Copy(nf, mf)
+	return fname
+}
